c719bc305b1549a111708b4860063a5ba7818a1f
DenisProkopchik <prokopchik.1996@mail.ru>
2020-09-28 22:38:35 +0300

    init: done reverse task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  n.toString()[0] === "-" ? n = Number(n.toString().slice(1)) : null;
+  return Number(n.toString().split("").reverse().join(''));
 }
